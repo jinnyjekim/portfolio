27317c7a43723c7e0e8c51488e6baf9fe2e8976c
jieunJinnyKim <first_jieun@naver.com>
2020-01-30 15:13:52 +0900

    Updates

---
+++ precache-manifest.d209a290ba1ea9beacffd659192e9647.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7a6a4b36ef6f86744ad48bbf984853d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "82b1f0ab4a8a97023127",
+    "url": "/portfolio/static/css/main.78abf235.chunk.css"
+  },
+  {
+    "revision": "f82dd9347b2e6073614f",
+    "url": "/portfolio/static/js/2.02703b9a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.02703b9a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82b1f0ab4a8a97023127",
+    "url": "/portfolio/static/js/main.a987cbf4.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  }
+]);
